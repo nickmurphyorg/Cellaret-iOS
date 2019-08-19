952ac980a85b9642410f8d1a5425d3baf817215c
nickmurphyorg <hello@nickmurphy.org>
2019-08-18 20:51:52 -0500

    Made connection with Semantics3 servers.

--- Cellaret/Other Controllers/ProductAPIController.swift
+++ Cellaret/Other Controllers/ProductAPIController.swift
@@ -13,8 +13,70 @@ import Keys
 class ProductAPIController {
     static let shared = ProductAPIController()
     
-    let endpoint = "products"
-    let upc = "682430400096"
+    let cellaretKeys = CellaretKeys()
+    let baseURL = URL(string: "https://api.semantics3.com/v1/")!
+    let oauthCredentials: OAuth1Swift!
+    let jsonDecoder = JSONDecoder()
     
+    private init() {
+        oauthCredentials = OAuth1Swift(consumerKey: cellaretKeys.semantics3Key, consumerSecret: cellaretKeys.semantics3Secret)
+    }
     
+    func getProduct(upc: String) {
+        let productURL = baseURL.appendingPathComponent("products")
+        let productData = ["upc": upc]
+        
+        guard let serializedProductData = serializeJSON(object: productData) else {
+            print("ProductAPIController - Could not serialize product data.")
+            
+            return
+        }
+        
+        let queryString = "q=" + serializedProductData
+        
+        var components = URLComponents(url: productURL, resolvingAgainstBaseURL: true)
+        components?.query = queryString
+        
+        guard let requestURL = components?.url else {
+            print("ProductAPIController - Request URL is nil.")
+            
+            return
+        }
+        
+        let _ = oauthCredentials.client.get(requestURL.absoluteString, success: { [weak self] (response) in
+            guard let weakSelf = self else {
+                return
+            }
+            
+            if let responseData = try? weakSelf.jsonDecoder.decode([String: String].self, from: response.data) {
+                // TODO: - Initialize model to pass data back to caller.
+                
+            }
+        }, failure: { (error) in
+            print("ProductAPIController - \(error)")
+        })
+    }
+}
+
+// Mark: - Helper Methods
+extension ProductAPIController {
+    func serializeJSON(object: Any) -> String? {
+        var serializedString: String?
+        
+        guard JSONSerialization.isValidJSONObject(object) else {
+            print("ProductAPIController - Object is not a valid JSON object.")
+            
+            return nil
+        }
+        
+        do {
+            let serializedJSONData: Data = try JSONSerialization.data(withJSONObject: object, options: .sortedKeys)
+            
+            serializedString = String(data: serializedJSONData, encoding: .utf8)
+        } catch let error {
+            print("ProductAPIController - \(error)")
+        }
+        
+        return serializedString
+    }
 }
---
+++ CellaretTests/ProductAPIControllerTests.swift
@@ -0,0 +1,28 @@
+//
+//  ProductAPIControllerTests.swift
+//  CellaretTests
+//
+//  Created by Nick Murphy on 8/18/19.
+//  Copyright © 2019 Nick Murphy. All rights reserved.
+//
+
+import XCTest
+
+class ProductAPIControllerTests: XCTestCase {
+    
+    let testUPC = "080480280024" // Grey Goose Vodka
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGetProduct() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
